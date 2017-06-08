d0dffbaa70704ab1091270f7404d4160ae88c704
jing <newzou9@gmail.com>
2017-06-07 23:08:49 -0400

    predict prices from last prices

--- predict_price.py
+++ predict_price.py
@@ -46,35 +46,54 @@ daily_price = genfromtxt('IBM.csv', delimiter=',', skip_header=1)
 dt = daily_price[:,0:1]
 price = daily_price[:,5] # adjusted last price
 
-target = price[1:] 
-price = price[:-1]
+total = 1000000.0
+cash = 1000000.0
+shares = 0
+n_horizon=4
+close_date = None
 
 n_prices = price.shape[0]
-n_train_instances = n_prices/n_steps
-n_prices = n_train_instances*n_steps 
 
-price = price[0:n_prices].reshape(n_train_instances,n_steps,n_inputs)
-target = target[0:n_prices].reshape(n_train_instances,n_steps,n_inputs)
-
-n_batches = n_train_instances/batch_size
+X_batch = np.zeros((1,n_steps, n_inputs))
+y_batch = np.zeros((1,n_steps, n_inputs))
 
 with tf.Session() as sess:
     sess.run(init)
-    for i in range(n_batches):
-        X_batch = price[i*batch_size:i*batch_size+batch_size]
-        y_batch = target[i*batch_size:i*batch_size+batch_size]
-
+    for i in range(n_prices-500, n_prices):
+        X_batch[0,:,0] = price[i+1-n_steps-n_horizon: i+1-n_horizon]
+        y_batch[0,:,0] = price[i+1-n_steps: i+1]
+      
         sess.run([train_op, loss], feed_dict={X:X_batch, y:y_batch})
-        y_pred = sess.run(outputs, feed_dict={X: X_batch})
-    
 
-        if i % 100 == 0:
-            mse = loss.eval(feed_dict={X:X_batch, y:y_batch})
-            print(i, "\tmse: ", mse)
-            plt.plot(X_batch[-1])
-            plt.plot(y_pred[-1])
-            plt.show()
+        y_pred = sess.run(outputs, feed_dict={X: y_batch})
+
+        target = y_pred[0,-1,0]
+        if target > price[i]*1.03:
+            if shares == 0:
+                shares = int(cash/price[i])
+                cash = cash - price[i]*shares
+                print("{} buy {} shares + cash {} = total {}".format(dt[i],
+                     shares, cash, cash+shares*price[i]))
+
+            close_date = i + n_horizon
+
+        if (close_date == i or target < price[i]*0.97) and shares > 0:
+            print("{} sell {} shares + cash {} = total {}".format(dt[i],
+                     shares, cash, cash+shares*price[i]))
+            cash += price[i]*shares
+            total = cash
+            shares = 0
+            
         
+    
+           # mse = loss.eval(feed_dict={X:X_batch, y:y_batch})
+           # print(i, "\tmse: ", mse)
+           # plt.plot(X_batch[-1])
+           # plt.plot(y_pred[-1])
+           # plt.show()
+        
+
+print("return: {}".format((total/1000000.0-1)*100))
     
         
 
